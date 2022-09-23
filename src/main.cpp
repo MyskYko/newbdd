5587326c7b932323ac14c2678a3f9451385e6607
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-09-23 16:02:37 -0700

    update main

--- src/main.cpp
+++ src/main.cpp
@@ -36,6 +36,49 @@ void Aig2Bdd(aigman const & aig, NewBdd::Man & bdd, std::vector<NewBdd::Node> & 
   }
 }
 
+int Bdd2Aig_rec(NewBdd::Man & bdd, aigman & aig, NewBdd::Node const & x, std::vector<int> & values) {
+  if(bdd.Id(x) == 0) {
+    return bdd.IsCompl(x);
+  }
+  if(values[bdd.Id(x)]) {
+    return values[bdd.Id(x)] ^ (int)bdd.IsCompl(x);
+  }
+  int v = (bdd.Var(x) + 1) << 1;
+  int i0 = Bdd2Aig_rec(bdd, aig, bdd.Else(x), values) ^ (int)bdd.IsCompl(x);
+  int i1 = Bdd2Aig_rec(bdd, aig, bdd.Then(x), values) ^ (int)bdd.IsCompl(x);
+  //std::cout << "node " << aig.nObjs + 2 << " var " << v << " then " << i1 << " else " << i0 << std::endl;
+  aig.vObjs.push_back(v);
+  aig.vObjs.push_back(i1);
+  aig.nGates++;
+  int j0 = aig.nObjs << 1;
+  aig.nObjs++;
+  aig.vObjs.push_back(v ^ 1);
+  aig.vObjs.push_back(i0);
+  aig.nGates++;
+  int j1 = aig.nObjs << 1;
+  aig.nObjs++;
+  aig.vObjs.push_back(j0 ^ 1);
+  aig.vObjs.push_back(j1 ^ 1);
+  aig.nGates++;
+  int r = (aig.nObjs << 1) ^ 1;
+  aig.nObjs++;
+  values[bdd.Id(x)] = r;
+  return r ^ (int)bdd.IsCompl(x);
+}
+
+void Bdd2Aig(NewBdd::Man & bdd, aigman & aig, std::vector<NewBdd::Node> const & vNodes) {
+  aig.clear();
+  aig.nPis = bdd.GetNumVars();
+  aig.nObjs = aig.nPis + 1;
+  aig.vObjs.resize(aig.nObjs * 2);
+  std::vector<int> values(bdd.GetNumObjs());
+  for(unsigned i = 0; i < vNodes.size(); i++) {
+    int j = Bdd2Aig_rec(bdd, aig, vNodes[i], values);
+    aig.vPos.push_back(j);
+    aig.nPos++;
+  }
+}
+
 int main(int argc, char ** argv) {
   aigman aig;
   aig.read(argv[1]);
@@ -44,5 +87,7 @@ int main(int argc, char ** argv) {
   std::vector<NewBdd::Node> vNodes;
   Aig2Bdd(aig, bdd, vNodes, 0);
   std::cout << bdd.CountNodes(vNodes) << std::endl;
+  Bdd2Aig(bdd, aig, vNodes);
+  aig.write("a.aig");
   return 0;
 }
