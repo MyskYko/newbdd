5a68e86d81ecbda0446d6db1afa6e554e1581a2e
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-09-26 16:42:47 -0700

    reo test

---
+++ test/aig2bdd_reotest.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+#include "AigBdd.h"
+
+int main(int argc, char ** argv) {
+  aigman aig;
+  aig.read(argv[1]);
+  aig.supportfanouts();
+  NewBdd::Man bdd(aig.nPis);
+  std::vector<NewBdd::Node> vNodes;
+  Aig2Bdd(aig, bdd, vNodes);
+  bdd.SetRef(vNodes);
+  bdd.Reo();
+  int count = bdd.CountNodes(vNodes);
+  std::vector<int> ordering;
+  bdd.GetOrdering(ordering);
+  NewBdd::Man bdd2(aig.nPis);
+  bdd2.SetInitialOrdering(ordering);
+  std::vector<NewBdd::Node> vNodes2;
+  Aig2Bdd(aig, bdd2, vNodes2);
+  int count2 = bdd2.CountNodes(vNodes2);
+  if(count == count2) {
+    std::cout << "[PASS] ";
+  } else {
+    std::cout << "[FAIL] ";
+  }
+  std::cout << count << " " << count2 << " " << argv[1] << std::endl;
+  return 0;
+}
