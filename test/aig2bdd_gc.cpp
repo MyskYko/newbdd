bf71e168b7a1a83e1818b51b7d613975bd025910
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-09-27 11:41:40 -0700

    warnings

--- test/aig2bdd_gc.cpp
+++ test/aig2bdd_gc.cpp
@@ -3,6 +3,10 @@
 #include "AigBdd.h"
 
 int main(int argc, char ** argv) {
+  if(argc == 1) {
+    std::cout << "Specify aig name" << std::endl;
+    return 0;
+  }
   aigman aig;
   aig.read(argv[1]);
   aig.supportfanouts();
