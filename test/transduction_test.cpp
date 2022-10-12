332b8a2161bac48c4c7d570fc00d31ba0320e206
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-10-12 13:41:09 -0700

    random test transduction

--- test/transduction_test.cpp
+++ test/transduction_test.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <random>
+#include <ctime>
 
 #include "Transduction.h"
 
@@ -9,19 +11,49 @@ int main(int argc, char ** argv) {
   }
   aigman aig;
   aig.read(argv[1]);
-  Transduction t(aig, 2);
+  Transduction t(aig, 0);
   int count = t.CountWires();
   int diff = 0;
-  // diff += t.TrivialMerge();
-  // diff += t.TrivialDecompose();
-  // diff += t.CspfEager();
-  // diff += t.Resub();
-  diff += t.ResubMono();
-  diff += t.TrivialMerge();
-  diff += t.Decompose();
-  std::cout << count << " " << diff << " " << t.CountWires() << std::endl;
-  if(count - diff != t.CountWires()) {
-    std::cout << "Wrong wire count!" << std::endl;
+  std::srand(std::time(nullptr));
+  int N = 100;
+  for(int i = 0; i < N; i++) {
+    int r = std::rand() % 10;
+    std::cout << "Test " << r << " : ";
+    switch(r) {
+    case 0:
+      diff += t.TrivialMerge();
+      break;
+    case 1:
+      diff += t.TrivialDecompose();
+      break;
+    case 2:
+      diff += t.Decompose();
+      break;
+    case 3:
+      diff += t.Cspf();
+      break;
+    case 4:
+      diff += t.CspfEager();
+      break;
+    case 5:
+      diff += t.Mspf();
+      break;
+    case 6:
+    case 7:
+      diff += t.Resub(r % 2);
+      break;
+    case 8:
+    case 9:
+      diff += t.ResubMono(r % 2);
+      break;
+    default:
+      return 1;
+    }
+    t.PrintStats();
+    if(count - diff != t.CountWires()) {
+      std::cout << "Wrong wire count!" << std::endl;
+      return 1;
+    }
   }
   t.Aig(aig);
   aig.write("b.aig");
