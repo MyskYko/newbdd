1243e7e1329a2442343e213b69c963ae4d2d7d49
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-10-13 17:46:59 -0700

    test transduction functionality

--- test/transduction_test.cpp
+++ test/transduction_test.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <random>
+#include <ctime>
 
 #include "Transduction.h"
 
@@ -9,67 +10,68 @@ int main(int argc, char ** argv) {
     std::cout << "Specify aig name" << std::endl;
     return 1;
   }
+  // read
   std::string aigname = argv[1];
   std::string outname = aigname + ".out.aig";
   aigman aig;
   aig.read(aigname);
+  // init
   int nodes = aig.nGates;
-  std::srand(123);
   int N = 100;
-  int M = 10;
-  for(int j = 0; j < M; j++) {
-    Transduction t(aig, 0);
-    int count = t.CountWires();
-    int diff = 0;
-    for(int i = 0; i < N; i++) {
-      int r = std::rand() % 10;
-      std::cout << "Test " << r << " : ";
-      switch(r) {
-      case 0:
-        diff += t.TrivialMerge();
-        break;
-      case 1:
-        diff += t.TrivialDecompose();
-        break;
-      case 2:
-        diff += t.Decompose();
-        break;
-      case 3:
-        diff += t.Cspf();
-        break;
-      case 4:
-        diff += t.CspfEager();
-        break;
-      case 5:
-        diff += t.Mspf();
-        break;
-      case 6:
-      case 7:
-        diff += t.Resub(r % 2);
-        break;
-      case 8:
-      case 9:
-        diff += t.ResubMono(r % 2);
-        break;
-      default:
-        return 1;
-      }
-      t.PrintStats();
-      if(!t.Verify()) {
-        std::cout << "Circuits are not equivalent!" << std::endl;
-        return 1;
-      }
-      if(count - diff != t.CountWires()) {
-        std::cout << "Wrong wire count!" << std::endl;
-        return 1;
-      }
-      if(t.CountNodes() < nodes) {
-        nodes = t.CountNodes();
-        aigman aig2;
-        t.Aig(aig2);
-        aig2.write(outname);
-      }
+  std::srand(time(NULL));
+  // transduction
+  Transduction t(aig, 0);
+  int count = t.CountWires();
+  int diff = 0;
+  for(int i = 0; i < N; i++) {
+    int r = std::rand() % 10;
+    std::cout << "Test " << r << " : ";
+    switch(r) {
+    case 0:
+      diff += t.TrivialMerge();
+      break;
+    case 1:
+      diff += t.TrivialDecompose();
+      break;
+    case 2:
+      diff += t.Decompose();
+      break;
+    case 3:
+      diff += t.Cspf();
+      break;
+    case 4:
+      diff += t.CspfEager();
+      break;
+    case 5:
+      diff += t.Mspf();
+      break;
+    case 6:
+    case 7:
+      diff += t.Resub(r % 2);
+      break;
+    case 8:
+    case 9:
+      diff += t.ResubMono(r % 2);
+      break;
+    default:
+      return 1;
+    }
+    t.PrintStats();
+    if(!t.Verify()) {
+      std::cout << "Circuits are not equivalent!" << std::endl;
+      return 1;
+    }
+    if(count - diff != t.CountWires()) {
+      std::cout << "Wrong wire count!" << std::endl;
+      return 1;
+    }
+    if(t.CountNodes() < nodes) {
+      nodes = t.CountNodes();
+      t.Aig(aig);
     }
   }
+  std::cout << "Results : nodes " << nodes << std::endl;
+  // write
+  aig.write(outname);
   return 0;
 }
