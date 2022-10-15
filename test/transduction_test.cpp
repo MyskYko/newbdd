478d300edd7796d72929fbe0c86e39d886889ee9
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-10-14 19:04:46 -0700

    print test pattern

--- test/transduction_test.cpp
+++ test/transduction_test.cpp
@@ -19,14 +19,24 @@ int main(int argc, char ** argv) {
   int nodes = aig.nGates;
   int N = 100;
   std::srand(time(NULL));
+  std::vector<int> tests;
+  for(int i = 0; i < N; i++) {
+    tests.push_back(std::rand() % 10);
+  }
+  std::cout << "Tests = {";
+  std::string delim;
+  for(unsigned i = 0; i < tests.size(); i++) {
+    std::cout << delim << tests[i];
+    delim = ", ";
+  }
+  std::cout << "}" << std::endl;
   // transduction
   Transduction t(aig, 0);
   int count = t.CountWires();
   int diff = 0;
-  for(int i = 0; i < N; i++) {
-    int r = std::rand() % 10;
-    std::cout << "Test " << r << " : ";
-    switch(r) {
+  for(unsigned i = 0; i < tests.size(); i++) {
+    std::cout << "Test " << tests[i] << " : ";
+    switch(tests[i]) {
     case 0:
       diff += t.TrivialMerge();
       break;
@@ -47,11 +57,11 @@ int main(int argc, char ** argv) {
       break;
     case 6:
     case 7:
-      diff += t.Resub(r % 2);
+      diff += t.Resub(tests[i] % 2);
       break;
     case 8:
     case 9:
-      diff += t.ResubMono(r % 2);
+      diff += t.ResubMono(tests[i] % 2);
       break;
     default:
       return 1;
