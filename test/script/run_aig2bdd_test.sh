fb8fe09ba8c2a00c16d0634cc733f107014261d4
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-10-16 03:57:07 -0700

    add scripts

---
+++ test/script/run_aig2bdd_test.sh
@@ -0,0 +1,20 @@
+i=0
+j=0
+
+for f in `cat mcnc-comb_small.txt`
+do
+    i=$((i+1))
+    a=`../oldbdd/build/example/aig2bdd -s -v 1 -p 4 $f`
+    a=`echo $a | sed -e 's/.*= \(.*\)Sum.*/\1/'`
+    b=`aig2bdd $f`
+    if [ $a == $b ]
+    then
+        #echo -n "[PASS] "
+        j=$((j+1))
+    #else
+        #echo -n "[FAIL] "
+    fi
+    #echo $a $b $f
+done
+
+echo "Summary : $j / $i passed"
---
+++ test/script/setup_oldbdd.sh
@@ -0,0 +1,5 @@
+cd ../oldbdd
+mkdir build
+cd build
+cmake -DBDD_EXAMPLE=ON ..
+make -j8
