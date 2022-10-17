348561eb738eca531d5d6c78b9aeff91f496787a
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-10-16 19:08:53 -0700

    update script for transduction

---
+++ test/script/run_transduction.sh
@@ -0,0 +1,12 @@
+if [ -z $1 ] || [ -z $2 ]
+then
+    echo "Specify list and directory"
+    exit 1    
+fi
+
+for f in `cat $1`
+do
+    g=`basename $f`
+    echo $f
+    transduction $f $2/$g | tee $2/$g.log
+done
--- test/script/run_transduction_large.sh
+++
@@ -1,7 +0,0 @@
-d="res"
-
-for f in `cat mcnc-pla_large.txt`
-do
-    g=`basename $f`
-    transduction_cspf $f $d/$g | tee $d/$g.log
-done
