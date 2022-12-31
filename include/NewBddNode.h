a4ed82f93c9d52560142cce8180d968a7512a08e
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2022-12-31 12:39:36 -0800

    inline node functions

--- include/NewBddNode.h
+++ include/NewBddNode.h
@@ -7,37 +7,127 @@ namespace NewBdd {
 
   class Node {
   public:
-    static Node Const0(Man * man);
-    static Node Const1(Man * man);
-    static Node IthVar(Man * man, var v);
-    static void SetRef(std::vector<Node> const & vNodes);
-    static bvar CountNodes(std::vector<Node> const & vNodes);
+    inline Node(Man * man, lit val) : man(man), val(val) {
+      man->IncRef(val);
+    }
+    inline Node() : man(NULL), val(0) {
+    }
+    inline Node(Node const & right) : man(right.man), val(right.val) {
+      if(man) {
+        man->IncRef(val);
+      }
+    }
+    inline ~Node() {
+      if(man) {
+        man->DecRef(val);
+      }
+    }
+    inline Node & operator=(Node const & right) {
+      if(this == &right) {
+        return *this;
+      }
+      if(man) {
+        man->DecRef(val);
+      }
+      man = right.man;
+      val = right.val;
+      if(man) {
+        man->IncRef(val);
+      }
+      return *this;
+    }
 
-    Node(Man * man, lit val);
-    Node();
-    Node(Node const & right);
-    ~Node();
-    Node & operator=(Node const & right);
+    inline var Var() const {
+      return man->Var(val);
+    }
+    inline bvar Id() const {
+      return man->Lit2Bvar(val);
+    }
+    inline bool IsCompl() const {
+      return man->LitIsCompl(val);
+    }
+    inline Node Then() const {
+      return Node(man, man->Then(val));
+    }
+    inline Node Else() const {
+      return Node(man, man->Else(val));
+    }
+    inline bool IsConst0() const {
+      return val == 0;
+    }
+    inline bool IsConst1() const {
+      return val == 1;
+    }
 
-    var Var() const;
-    bvar Id() const;
-    bool IsCompl() const;
-    Node Then() const;
-    Node Else() const;
-    bool IsConst0() const;
-    bool IsConst1() const;
+    inline double OneCount() const {
+      return man->OneCount(val);
+    }
+    inline double ZeroCount() const {
+      return man->OneCount(man->LitNot(val));
+    }
 
-    double OneCount() const;
-    double ZeroCount() const;
+    inline Node operator~() const {
+      return Node(man, man->LitNot(val));
+    }
+    inline Node operator^(bool c) const {
+      return c? ~*this: *this;
+    }
+    inline Node operator&(Node const & other) const {
+      return Node(man, man->And(val, other.val));
+    }
+    inline Node operator|(Node const & other) const {
+      return Node(man, man->LitNot(man->And(man->LitNot(val), man->LitNot(other.val))));
+    }
+    inline Node operator^(Node const & other) const {
+      Node z0 = Node(man, man->And(man->LitNot(val), other.val));
+      Node z1 = Node(man, man->And(val, man->LitNot(other.val)));
+      return z0 | z1;
+    }
 
-    Node operator~() const;
-    Node operator^(bool c) const;
-    Node operator&(Node const & other) const;
-    Node operator|(Node const & other) const;
-    Node operator^(Node const & other) const;
+    inline bool operator==(Node const & other) const {
+      return man == other.man && val == other.val;
+    }
+    inline bool operator!=(Node const & other) const {
+      return !(*this == other);
+    }
 
-    bool operator==(Node const & other) const;
-    bool operator!=(Node const & other) const;
+    static inline Node Const0(Man * man) {
+      return Node(man, man->Const0());
+    }
+    static inline Node Const1(Man * man) {
+      return Node(man, man->Const1());
+    }
+    static inline Node IthVar(Man * man, var v) {
+      return Node(man, man->IthVar(v));
+    }
+    static inline void SetRef(std::vector<Node> const & vNodes) {
+      if(vNodes.empty()) {
+        return;
+      }
+      Man * man = vNodes[0].man;
+      std::vector<lit> vLits(vNodes.size());
+      for(size i = 0; i < vNodes.size(); i++) {
+        if(man != vNodes[i].man) {
+          throw std::logic_error("Nodes do not share the same manager");
+        }
+        vLits[i] = vNodes[i].val;
+      }
+      man->SetRef(vLits);
+    }
+    static inline bvar CountNodes(std::vector<Node> const & vNodes) {
+      if(vNodes.empty()) {
+        return 0;
+      }
+      Man * man = vNodes[0].man;
+      std::vector<lit> vLits(vNodes.size());
+      for(size i = 0; i < vNodes.size(); i++) {
+        if(man != vNodes[i].man) {
+          throw std::logic_error("Nodes do not share the same manager");
+        }
+        vLits[i] = vNodes[i].val;
+      }
+      return man->CountNodes(vLits);
+    }
 
   private:
     Man * man;
