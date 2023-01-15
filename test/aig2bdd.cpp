87917bd33341b24520985c27a20f442bbf50694c
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2023-01-15 07:30:02 -0800

    update aig2bdd

--- test/aig2bdd.cpp
+++ test/aig2bdd.cpp
@@ -1,18 +1,32 @@
-#include <iostream>
+#include <argparse/argparse.hpp>
 
 #include "AigBdd.h"
 
+using namespace std;
+
 int main(int argc, char ** argv) {
-  if(argc == 1) {
-    std::cout << "Specify aig name" << std::endl;
+  argparse::ArgumentParser ap("aig2bdd");
+  ap.add_argument("input");
+  ap.add_argument("-g", "--garbage_collect").default_value(false).implicit_value(true);
+  ap.add_argument("-r", "--reorder").default_value(false).implicit_value(true);
+  try {
+    ap.parse_args(argc, argv);
+  }
+  catch (const runtime_error& err) {
+    cerr << err.what() << endl;
+    cerr << ap;
     return 1;
   }
+  string aigname = ap.get<string>("input");
+  bool fGc = ap.get<bool>("-g");
+  bool fReo = ap.get<bool>("-r");
   aigman aig;
-  aig.read(argv[1]);
+  aig.read(aigname);
   aig.supportfanouts();
   NewBdd::Man bdd(aig.nPis);
-  std::vector<NewBdd::Node> vNodes;
+  bdd.SetParameters(fGc, fReo? 10: -1);
+  vector<NewBdd::Node> vNodes;
   Aig2Bdd(aig, bdd, vNodes);
-  std::cout << NewBdd::Node::CountNodes(vNodes) << std::endl;
+  cout << NewBdd::Node::CountNodes(vNodes) << endl;
   return 0;
 }
