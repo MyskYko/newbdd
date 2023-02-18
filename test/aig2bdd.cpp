a03ea7e6ef77399da91939f426f1304f0113f966
Yukio Miyasaka <yoyuohlhjl@yahoo.co.jp>
2023-02-18 12:16:54 -0800

    gbc option

--- test/aig2bdd.cpp
+++ test/aig2bdd.cpp
@@ -8,7 +8,7 @@ int main(int argc, char ** argv) {
   argparse::ArgumentParser ap("aig2bdd");
   ap.add_argument("input");
   ap.add_argument("-v", "--verbose").default_value(0).scan<'i', int>();
-  ap.add_argument("-g", "--garbage_collect").default_value(false).implicit_value(true);
+  ap.add_argument("-g", "--garbage_collect").default_value(0).scan<'i', int>();
   ap.add_argument("-r", "--reorder").default_value(false).implicit_value(true);
   try {
     ap.parse_args(argc, argv);
@@ -20,13 +20,13 @@ int main(int argc, char ** argv) {
   }
   string aigname = ap.get<string>("input");
   int nVerbose = ap.get<int>("-v");
-  bool fGc = ap.get<bool>("-g");
+  int nGbc = ap.get<int>("-g");
   bool fReo = ap.get<bool>("-r");
   aigman aig;
   aig.read(aigname);
   aig.supportfanouts();
   NewBdd::Man bdd(aig.nPis, 0, nVerbose);
-  bdd.SetParameters(fGc, fReo? 10: -1);
+  bdd.SetParameters(nGbc, fReo? 10: -1);
   vector<NewBdd::Node> vNodes;
   Aig2Bdd(aig, bdd, vNodes);
   cout << NewBdd::Node::CountNodes(vNodes) << endl;
